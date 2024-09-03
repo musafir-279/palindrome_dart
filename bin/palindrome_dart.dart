c472711dd1cbf5bbb6e402fc6ee26f4fe530911f
unknown <you@example.com>
2024-09-03 11:15:23 +0530

    first commit

---
+++ bin/palindrome_dart.dart
@@ -0,0 +1,30 @@
+
+import 'dart:io';
+
+void main() {
+  print("type anynword");
+
+  String? input = stdin.readLineSync();
+
+  if (input != null && input.isNotEmpty) {
+    if (ispalindrome(input)) {
+      print("'$input''is a palindrome.");
+    } else {
+      print("'$input' is not a palindrome.");
+    }
+
+  }
+}
+
+bool ispalindrome(String input){
+  String cleanedinput = input.replaceAll ('', '').toLowerCase();
+  int length = cleanedinput.length;
+
+  for (int i =0; i < length ~/ 2; i++) {
+    if (cleanedinput[i] != cleanedinput[length - i -1]){
+      return false;
+
+    }
+  }
+  return true;
+}
---
+++ test/palindrome_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:palindrome_dart/palindrome_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
